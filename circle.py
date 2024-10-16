1d884a9a527be0b1bbc7741fbe2d4bb0bcd166c2
Ivan Gorinov <153944563+1eanq@users.noreply.github.com>
2024-10-16 11:51:26 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -1,10 +1,10 @@
 import math
 
-
+//Принимает радиус круга, возвращает площадь круга
 def area(r):
     return math.pi * r * r
 
-
+//Принимает радиус окружности, возвращает длинну окружности
 def perimeter(r):
     return 2 * math.pi * r
 
